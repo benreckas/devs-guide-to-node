db7501ff74be2c46ba00ed3d1d5fdc17f0d44006
benreckas <benreckas@gmail.com>
2018-03-27 10:10:42 -0700

    Taking in prompted address from user

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,34 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+      a: {
+        demand: true,
+        alias: 'address',
+        describe: 'Address to fetch weather',
+        string: true
+      }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+  const encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=3826%20Chessa%20Ln%20Clovis',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
-}, (res, err) => {
-  console.log(JSON.stringify(res, undefined, 2));
+}, async (err, res) => {
+  try {
+    if(res.statusCode === 200) {
+      console.log(`Address: ${res.body.results[0].formatted_address}`)
+      console.log(`Lat: ${res.body.results[0].geometry.location.lat}`)
+      console.log(`Lng: ${res.body.results[0].geometry.location.lng}`)
+    }
+  } catch ( err ) {
+    console.log('Invalid Address')
+  }
+
+  // console.log(JSON.stringify(res, undefined, 2));
 });
